dd4d2da8377053c20ec4391c28ec51cda8db586d
taoveweb <taoveweb@gmail.com>
2016-08-19 09:50:56 +0800

    all

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  openId: {type:string,required:"openId 是必颂数"},
+  openId: {type:String,required:"openId 是必颂数"},
   createdAt: {type:Date,required:"openId 是必颂数"},
   conversationCount: {type:Number,default :0}
 });
