b9eeb7c3a28e6bca2e2488ee15649d44eaf5fc51
taoveweb <taoveweb@gmail.com>
2016-08-19 09:53:19 +0800

    all

--- app/models/conversation.js
+++ app/models/conversation.js
@@ -3,12 +3,12 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
-var UserSchema = new Schema({
+var ConversationSchema = new Schema({
   user: {type:Schema.ObjectId,ref:"User",required:"user是必填字段"},
   question: {type:String,required:"question是必填字段"},
   answer: {type:String,required:"question是必填字段"},
   createdAt: {type:Date,required:"openId 是必颂数"},
 });
 
-mongoose.model('User', UserSchema);
+mongoose.model('Conversation', ConversationSchema);
 
